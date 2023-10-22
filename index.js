8b4273be62ab4bb585032ba7b092a6f6fe496c1a
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2023-10-22 18:00:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,7 @@ const PORT = process.env.PORT || 8081;
 app.get('/', async (req, res) => {
   const username = req.query.username || 'myogeshchavan97';
   try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
+    repos='Its working!.';
 
     res.send(repos);
   } catch (error) {
