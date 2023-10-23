ea7c1a813482d7c8135f18611276331cbd2eeed7
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2023-10-23 05:38:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ const app = express();
 const PORT = process.env.PORT || 8081;
 const fs= require('fs');
 
-const download = (url, dest, cb) => {
-axios.get(url, {responseType: "stream"} )  
-.then(response => {  
-// Saving file to working directory  
-    response.data.pipe(fs.createWriteStream(dest));  
-})  
-    .catch(error => {  
-    console.log(error);  
-}); 
-}
 
 app.get('/file', async (req, res) => {
   const u = req.query.n;
@@ -31,8 +21,17 @@ app.get('/download',async (req,res) => {
   try{
   const na=url.split('/');
   const path='/files/'+na[na.length-2];
-    x=await download(url,path);
-    res.send(x);
+ consolelog(path);
+axios.get(url, {responseType: "stream"} )  
+.then(response => {  
+// Saving file to working directory  
+    response.data.pipe(fs.createWriteStream(path));  
+    res.send(path);
+})  
+    .catch(error => {  
+    console.log(error);  
+        res.send('err');
+});  
   } catch (error) {
     res.status(400).send('Err');
   }
